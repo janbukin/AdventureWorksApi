a696e11d75fc26f8687fb10174349a6fa9e2bc6d
Jan Bukin <jdbukin@gmail.com>
2018-11-19 16:34:42 +0300

    Added CRUD for Product

---
+++ AdventureWorksApi/Models/ProductDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AdventureWorksApi.Models
+{
+    public class ProductDto
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string ProductNumber { get; set; }
+
+        public bool MakeFlag { get; set; }
+
+        public bool FinishedGoodsFlag { get; set; }
+
+        public short SafetyStockLevel { get; set; }
+
+        public short ReorderPoint { get; set; }
+
+        public decimal StandartCost { get; set; }
+
+        public decimal ListPrice { get; set; }
+
+        public int DaysToManufacture { get; set; }
+
+        public DateTime SellStartDate { get; set; }
+
+        public Guid rowguid { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
